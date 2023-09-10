9d7bd150bb6f1e6c5680c89ec54f9730f4b0c0b5
onflyer <salkaorama@gmail.com>
2023-09-10 13:23:43 +0200

    Initial Commit

---
+++ GlobalNavigation (SwiftUI)/GlobalNavigation__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalNavigation__SwiftUI_App.swift
+//  GlobalNavigation (SwiftUI)
+//
+//  Created by Aleksandar Milidrag on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GlobalNavigation__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
